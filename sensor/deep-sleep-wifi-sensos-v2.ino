a0ce5a2d5d81e228579bf8183425ecf6411b8777
josema <josemariafabiano@gmail.com>
2022-04-27 22:05:30 -0300

    added ky018

--- sensor/deep-sleep-wifi-sensos-v2.ino
+++ sensor/deep-sleep-wifi-sensos-v2.ino
@@ -1,13 +1,28 @@
 /**
    ESP8266 + DHT11 + DS18B20 + Deep-sleep mode
 */
-
+#include <string>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <DHT.h>
-#include <OneWire.h>                
+#include <OneWire.h>
 #include <DallasTemperature.h>
+
+
+// Sensors
+
+//    DS18B20
+OneWire ourWire(4);                      //Se establece el pin 2  como bus OneWire
+DallasTemperature sensors(&ourWire);     //Se declara una variable u objeto para nuestro sensor
+
+//    DHT11
+const byte dhtPin = 5;
+#define DHTTYPE DHT11
+DHT dht(dhtPin, DHTTYPE);
+
+//    KY-018
+#define PIN_ANALOG A0
 
 // WiFi credentials.
 const char* WIFI_SSID = "ba-sing-se 2.4 GHZ";
@@ -15,26 +30,15 @@ const char* WIFI_PASS = "LotoBlanco";
 const char* serverName = "http://192.168.0.33:9000/sensor";
 
 // Client
-  WiFiClient client;
-  HTTPClient http;
-
-// Sensors
-//    DS18B20
-OneWire ourWire(4);                      //Se establece el pin 2  como bus OneWire
-DallasTemperature sensors(&ourWire);     //Se declara una variable u objeto para nuestro sensor
-//     sensor DHT11
-const byte dhtPin = 5;
-#define DHTTYPE DHT11
-DHT dht(dhtPin, DHTTYPE);
+WiFiClient client;
+HTTPClient http;
 
 
-// Connect to Wifi.
 void connect() {
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-
   // WiFi fix: https://github.com/esp8266/Arduino/issues/2186
   WiFi.persistent(false);
   WiFi.mode(WIFI_OFF);
@@ -64,81 +68,116 @@ void connect() {
   Serial.println(WiFi.localIP());
 }
 
-void notify(char message[]) {
-  http.begin(client, serverName);
-  http.addHeader("Content-Type", "application/json");
-
+void notify(char* message) {
   int httpResponseCode = http.POST(message);
   Serial.print("Posted :");
   Serial.println(message);
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
-
-  // Free resources
-  http.end();
 }
 
-void dht11Notify(){
-   float dht11_temp, dht11_hum;
+void dht11Reading(char* buffer) {
+  String sensor = "dht11";
+  float dht11_temp, dht11_hum;
 
-   dht.begin();
-   dht11_hum = dht.readHumidity();
-   dht11_temp = dht.readTemperature();
+  dht.begin();
+  dht11_hum = dht.readHumidity();
+  dht11_temp = dht.readTemperature();
 
-   if (isnan(dht11_hum) || isnan(dht11_temp)) {
-   Serial.println(F("Failed to read from DHT sensor!"));
-   return;
+  if (!isnan(dht11_hum) && !isnan(dht11_temp)) {
+    sprintf_P(buffer, "{\"%s\":{\"temp\":%d.%02d,\"hum\":%d.%02d}}", (String)sensor, (int)dht11_temp, (int)(dht11_temp * 100) % 100, (int)dht11_hum, (int)(dht11_hum * 100) % 100);
+
+  } else {
+    Serial.println("Failed to read from DHT11 sensor!");
+    sprintf_P(buffer, "{\"sensorFailure\":{\"name\":%s}}", (String)sensor);
+  }
 }
 
-   char buffer[100] = "";
-   sprintf_P(buffer, "{\"dht11\":{\"temp\":%d.%02d,\"hum\":%d.%02d}}", (int)dht11_temp, (int)(dht11_temp*100)%100, (int)dht11_hum, (int)(dht11_hum*100)%100);
-   notify(buffer);
+void ky018Reading(char* buffer) {
+  String sensor = "ky018";
 
+  int measureMean = 0;
+  int measureCont = 0;
+  int successfulMeasures = 30;
+
+  for (int i = 0; i < 30; ++i) {
+    int measure = analogRead(PIN_ANALOG);
+
+    if (isnan(measure)) {
+      successfulMeasures --;
+    }else {
+      measureCont += measure;
+    }
+  }
+
+  if (successfulMeasures > 25) {
+    measureMean = measureCont / successfulMeasures;
+    sprintf_P(buffer, "{\"%s\":{\"light\":%d}}", sensor, (int)measureMean);
+
+  } else {
+    Serial.println("Failed to read from KY-018 sensor!");
+    sprintf_P(buffer, "{\"sensorFailure\":{\"name\":%s}}", (String)sensor);
+  }
 }
 
-void ds18b20Notify(){
-   float ds18b20_temp;
+void ds18b20Reading(char* buffer) {
+  String sensor = "ds18b20";
+  float ds18b20_temp;
 
-   sensors.begin();
-   sensors.requestTemperatures();
-   ds18b20_temp= sensors.getTempCByIndex(0);
+  sensors.begin();
+  sensors.requestTemperatures();
+  ds18b20_temp = sensors.getTempCByIndex(0);
 
-   if (isnan(ds18b20_temp)) {
-      Serial.println(F("Failed to read from DS18B20 sensor!"));
-      return;
-   }
+  if (!isnan(ds18b20_temp)) {
+    sprintf_P(buffer, "{\"ds18b20\":{\"temp\":%d.%02d}}", (int)ds18b20_temp, (int)(ds18b20_temp * 100) % 100);
 
-   char buffer[50] = "";
-   sprintf_P(buffer, "{\"ds18b20\":{\"temp\":%d.%02d}}", (int)ds18b20_temp, (int)(ds18b20_temp*100)%100);
-   
-   notify(buffer);
+  } else {
+    Serial.println("Failed to read from DS18B20 sensor!");
+    sprintf_P(buffer, "{\"sensorFailure\":{\"name\":%s}}", (String)sensor);
+  }
 }
 
-void serialInitialization(){
-   Serial.begin(9600);
-   Serial.setTimeout(2000);
+void serialInitialization() {
+  Serial.begin(9600);
+  Serial.setTimeout(2000);
 
-   // Wait for serial to initialize.
-   while (!Serial) {}
-
-   Serial.println("Device Started");
-   Serial.println("-------------------------------------");
+  // Wait for serial to initialize.
+  while (!Serial) {}
+  Serial.println("Device Started");
+  Serial.println("-------------------------------------");
 }
 
-void deepSleep(){
-   Serial.println("Running Deep Sleep Firmware!");
-   Serial.println("Going into deep sleep for 60 seconds");
-   Serial.println("-------------------------------------");
-   Serial.println("");
-   ESP.deepSleep(60e6);  // 60e6 is 60 microseconds
+void deepSleep() {
+  Serial.println("Running Deep Sleep Firmware!");
+  Serial.println("Going into deep sleep for 60 seconds");
+  Serial.println("-------------------------------------");
+  Serial.println("");
+  ESP.deepSleep(60e6);  // 60e6 is 60 microseconds
 }
+
 
 void setup() {
-   serialInitialization();
-   connect();
-   ds18b20Notify();
-   dht11Notify();
-   deepSleep();
+  http.begin(client, serverName);
+  http.addHeader("Content-Type", "application/json");
+
+  serialInitialization();
+  connect();
+
+  char dht11Payload[100] = "";
+  dht11Reading(dht11Payload);
+  notify(dht11Payload);
+
+  char ky018Payload[100] = "";
+  ky018Reading(ky018Payload);
+  notify(ky018Payload);
+
+  char ds18b20Payload[100] = "";
+  ds18b20Reading(ds18b20Payload);
+  notify(ds18b20Payload);
+
+  // Free up resources
+  http.end();
+  deepSleep();
 }
 
 void loop() {
